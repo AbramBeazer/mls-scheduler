98f633292d8afa0472efc267df0dd10edfdcd2b9
abrambeazer <abrambeazer@gmail.com>
2023-08-28 16:00:38 -0700

    Add method to schedule games within the same conference (each team faces each other team once at home and away).

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -13,14 +16,43 @@ public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
+    private static final int GAMES_PER_SEASON = 34;
+
     public static void main(String[] args) {
         try (InputStream stream = Main.class.getClassLoader().getResourceAsStream("mls.json")) {
             Objects.requireNonNull(stream, "Input stream cannot be null");
             League league = MAPPER.readValue(stream.readAllBytes(), League.class);
 
-            System.out.println(MAPPER.writeValueAsString(league));
+            List<Team> teams = league.getConferences()
+                .stream()
+                .flatMap(c -> c.getTeams().stream())
+                .collect(Collectors.toList());
+
+            List<Matchup> matchups = getConferenceMatchups(league);
+            matchups.addAll(getInterconferenceMatchups(league));
+
         } catch (IOException e) {
             LOG.error(e.getMessage());
         }
+    }
+
+    private static List<Matchup> getConferenceMatchups(League league) {
+        List<Matchup> matchups = new ArrayList<>();
+        for (Conference conf : league.getConferences()) {
+            for (Team team : conf.getTeams()) {
+                for (Team otherTeam : conf.getTeams()) {
+                    if (!team.equals(otherTeam)) {
+                        matchups.add(new Matchup(team, otherTeam));
+                    }
+                }
+            }
+        }
+        return matchups;
+    }
+
+    private static List<Matchup> getInterconferenceMatchups(League league) {
+        List<Matchup> matchups = new ArrayList<>();
+
+        return matchups;
     }
 }
